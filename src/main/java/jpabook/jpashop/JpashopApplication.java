cb2bc160a86fd157f1d5efd2a8aaff1ebe7c0a1b
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-03 23:43:49 +0900

    0103

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setH("hello");
+		String data = hello.getH();
+		System.out.println(data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
