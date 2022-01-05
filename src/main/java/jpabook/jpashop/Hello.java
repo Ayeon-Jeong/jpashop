9a1fd86cfe37a9a53b5d5411ffd3fc3916cf999a
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-06 00:01:06 +0900

    0106

--- src/main/java/jpabook/jpashop/Hello.java
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -3,14 +3,9 @@ package jpabook.jpashop;
 import lombok.Getter;
 import lombok.Setter;
 
-<<<<<<< HEAD
 @Getter @Setter
-public class Hello {
-    private String h;
-=======
 public class Hello {
 
     @Getter @Setter
     public String h;
->>>>>>> bf435117f0148c5b4ad6410e2a6bc4a0931bf3b7
 }
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Hello hello = new Hello();
-		hello.setH("hello");
-		String data = hello.getH();
-		System.out.println(data);
-=======
->>>>>>> bf435117f0148c5b4ad6410e2a6bc4a0931bf3b7
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
