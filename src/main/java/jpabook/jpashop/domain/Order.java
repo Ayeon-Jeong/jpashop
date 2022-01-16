cd37b55a7fecb5e5f8cdbbf4c4a841ba66a151ed
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-16 23:40:05 +0900

    0116

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,16 +17,17 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     // member와 관계 세팅
     // fk 사용시 연관 관계의 주인을 정해야 한다.
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -37,4 +38,21 @@ public class Order {
     // 주문상태: ORDER, CANCEL
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    // ==연관관계 메서드== //
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
